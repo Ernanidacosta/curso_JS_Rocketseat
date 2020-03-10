6e2d51f29d5301ab27023c8d03d28f5fd7f93f42
Ernani da Costa <ernanidacosta@gmail.com>
2020-03-10 10:32:58 -0300

    implementado js

---
+++ tarefas.js
@@ -0,0 +1,10 @@
+var listElement = document.querySelector('#app ul');
+var inputElement = document.querySelector('#app input');
+var buttonElement = document.querySelector('#app button');
+
+
+var tarefas = [
+    'estudar cloud functions',
+    'estudar firebase',
+    'estudar cloud run - knative'
+];
