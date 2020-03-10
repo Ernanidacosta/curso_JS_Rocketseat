a61ae1744249f67587c075e2d68f1e7e0c738347
Ernani da Costa <ernanidacosta@gmail.com>
2020-03-10 11:32:43 -0300

    criado addTarefa e outras melhorias

--- tarefas.js
+++ tarefas.js
@@ -10,12 +10,32 @@ var tarefas = [
 ];
 
 function renderTarefas(){
+    listElement.innerHTML = '';
+
     for (tarefa of tarefas){
         var tarefaElement = document.createElement('li');
         var tarefasText = document.createTextNode(tarefa);
+        
+        var linkElement = document.createElement('a');
+        linkElement.setAttribute('href', '#');
 
+        var linkText = document.createTextNode('Excluir');
+
+        linkElement.appendChild(linkText);
+        
         tarefaElement.appendChild(tarefasText);
+        tarefaElement.appendChild(linkElement);
         listElement.appendChild(tarefaElement);
     }
 }
 renderTarefas();
+
+function addTarefa(){
+    var tarefasText = inputElement.value;
+
+    tarefas.push(tarefasText);
+    inputElement.value = '';
+    renderTarefas();
+}
+
+buttonElement.onclick = addTarefa;
