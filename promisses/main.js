138712aecfadd9ce574c512e29a535c13ddce973
Ernani da Costa <ernanidacosta@gmail.com>
2020-03-11 18:14:24 -0300

    created promisses

--- promisses/main.js
+++ promisses/main.js
@@ -2,5 +2,23 @@ var minhaPromisse = function(){
     return new Promise(function(resolve, reject){
         var xhr = new XMLHttpRequest();
         xhr.open('GET', 'https://api.github.com/users/ernanidacosta');
+        xhr.send(null);
+
+        xhr.onreadystatechange = function(){
+            if (xhr.readyState === 4){
+                if (xhr.status === 200){
+                    resolve(JSON.parse(xhr.responseText));
+                }else{
+                    reject('Erro na Requisição')
+                }
+            }
+        }
     });
 }
+minhaPromisse()
+    .then(function(response) {
+        console.log(response);
+    })
+    .catch(function(error) {
+        console.warn(error);
+    });
